c5cadb992f2716ae1182db9224949c3d7aca4c66
Jan Friedrich <janfri26@gmail.com>
2024-03-05 18:42:49 +0100

    Cleanup repository

--- lib/ebookbinder.rb
+++ lib/ebookbinder.rb
@@ -1,5 +1,6 @@
 # encoding: utf-8
 require 'rake'
+require 'rake/clean'
 require 'yaml'
 
 module Ebookbinder
--- lib/ebookbinder/ebook_base.rb
+++ lib/ebookbinder/ebook_base.rb
@@ -1,7 +1,5 @@
 # encoding: utf-8
 require_relative '../ebookbinder'
-
-require 'rake/clean'
 require 'singleton'
 
 module Ebookbinder
